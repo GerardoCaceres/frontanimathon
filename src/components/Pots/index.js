541adff78640088d652e7ea781db86e9380edc0a
Mel Miranda <melania.miranda@mercadolibre.com>
2023-03-20 16:22:14 -0300

    component pots base

---
+++ src/components/Pots/index.js
@@ -0,0 +1,48 @@
+import alcancia from '../../images/pots/alcancia.svg'
+import corazon from '../../images/pots/corazon.svg';
+import avion from '../../images/pots/avion.svg';
+import casa from '../../images/pots/casa.svg';
+import agenda from '../../images/pots/agenda.svg';
+import './style.css';
+
+function Pots() {
+  return (
+    <div className="pots">
+
+      <div className="header">
+        <img src={alcancia} className="pots-icon" alt="alcancia" />
+        <p>Você guardou R$ 1.250 entre estas<br/> Reservas neste mês</p>
+      </div>
+
+      <div className="list">
+        <ul>
+          <li>  
+            <img src={corazon} className="pots-icon" alt="alcancia" /> 
+            <p>Lua de mel</p>
+            <span> + R$ 1.0000</span>
+          </li>
+          <li>  
+            <img src={avion} className="pots-icon" alt="alcancia" /> 
+            <p>tulum</p>
+            <span> + R$ 200</span>
+          </li>
+          <li>  
+            <img src={casa} className="pots-icon" alt="alcancia" /> 
+            <p>Reforma da casa e praia</p>
+            <span> + R$ 50</span>
+          </li>
+        </ul>
+      </div>
+
+
+
+      <div className="footer">
+        <div className='image'><img src={agenda} className="pots-icon" alt="alcancia" /></div>
+        <p>Sabia que é possível programar reservas? <br/> É prático e perfeito para ajudar a focar!</p>
+        <button>Ir para Reservas</button>
+      </div>
+    </div>
+  );
+}
+
+export default Pots;
