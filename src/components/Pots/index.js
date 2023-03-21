24d62b7e71d662fdf3f2fdf16f26d5bcbc06fa8f
melinatabor99 <melina.tabor@mercadolibre.com>
2023-03-21 09:03:26 -0300

    wave animation and bounce icons hover animation

--- src/components/Pots/index.js
+++ src/components/Pots/index.js
@@ -8,37 +8,42 @@ import './style.css';
 function Pots() {
   return (
     <div className="pots">
-
       <div className="header">
         <img src={alcancia} className="pots-icon" alt="alcancia" />
-        <p>Você guardou R$ 1.250 entre estas<br/> Reservas neste mês</p>
+        <p>Você guardou R$ 1.250 entre estas<br /> Reservas neste mês</p>
       </div>
 
       <div className="list">
         <ul>
-          <li>  
-            <img src={corazon} className="pots-icon" alt="alcancia" /> 
-            <p>Lua de mel</p>
+          <li>
+            <div className="left-column">
+              <img src={corazon} className="pots-icon" alt="alcancia" />
+              <p>Lua de mel</p>
+            </div>
             <span> + R$ 1.0000</span>
           </li>
-          <li>  
-            <img src={avion} className="pots-icon" alt="alcancia" /> 
-            <p>tulum</p>
+          <li>
+            <div className="left-column">
+              <img src={avion} className="pots-icon" alt="alcancia" />
+              <p>tulum</p>
+            </div>
             <span> + R$ 200</span>
           </li>
-          <li>  
-            <img src={casa} className="pots-icon" alt="alcancia" /> 
-            <p>Reforma da casa e praia</p>
+          <li>
+            <div className="left-column">
+              <img src={casa} className="pots-icon" alt="alcancia" />
+              <p>Reforma da casa e praia</p>
+            </div>
             <span> + R$ 50</span>
           </li>
         </ul>
-      </div>
+        </div>
 
 
 
       <div className="footer">
         <div className='image'><img src={agenda} className="pots-icon" alt="alcancia" /></div>
-        <p>Sabia que é possível programar reservas? <br/> É prático e perfeito para ajudar a focar!</p>
+        <p>Sabia que é possível programar reservas? <br /> É prático e perfeito para ajudar a focar!</p>
         <button>Ir para Reservas</button>
       </div>
     </div>
