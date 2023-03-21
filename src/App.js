5670716f8b419772fd24671ac14da15127d023f9
Esteban Ulke <esteban.ulke@mercadolibre.com>
2023-03-21 09:24:08 -0300

    Revert "feat: use web animation API"
    This reverts commit 59639788ac09c019910c6b6e18ac3bf4d523f0fd.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { useCallback, useEffect, useRef, useState } from 'react';
-
-function useAnimate({ refOpt }) {
-  const animRef = useRef()
-  const refVar = useRef(null);
-  const ref = refOpt || refVar
-
-  const getAnimation = useCallback(() => animRef.current, []);
-
-  const animate = useCallback(({ keyframes, animationOptions, autoPlay }) => {
-    if (!ref.current || !keyframes) return
-    animRef.current = ref.current.animate(keyframes, animationOptions)
-    const { current: anim } = animRef;
-
-    if (!autoPlay) anim.pause()
-  }, ref)
-
-  return { animate, ref, getAnimation }
-}
 
 function App() {
-  const { animate, ref, getAnimation } = useAnimate({})
-
-  const rotate = [
-    { transform: "rotate(0)" },
-    { transform: "rotate(360deg)" },
-  ];
-
-  const timming = {
-    duration: 3000,
-    iterations: Infinity,
-  }
-
-  useEffect(() => {
-    animate({ keyframes: rotate, animationOptions: timming, autoPlay: true })
-  }, [])
-
-  const play = () => {
-    getAnimation().play();
-  }
-
-  const pause = () => {
-    getAnimation().pause();
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <img ref={ref} src={logo} className="App-logo" alt="logo" />
-        <button onClick={play}>Play</button>
-        <button onClick={pause}>Pause</button>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
