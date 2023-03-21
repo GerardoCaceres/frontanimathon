d156f9df193da8b64d2b6b884c81c8c73fd394a1
Esteban Ulke <esteban.ulke@mercadolibre.com>
2023-03-21 09:49:48 -0300

    Revert "Merge branch 'feature/team-fafrafa' into feature/tete"
    This reverts commit b66af4fa9f515cc3536feb20d6c2b6bab7798ee4, reversing
    changes made to 6bd74d4f149ffc0442646740479ad36380318fff.

--- src/images/svg/Ethereum.jsx
+++
@@ -1,7 +0,0 @@
-const Ethereum = () => (
-    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9 17.5C13.7104 17.5 17.5 13.7104 17.5 9C17.5 4.28956 13.7104 0.5 9 0.5C4.28956 0.5 0.5 4.28956 0.5 9C0.5 13.7104 4.28956 17.5 9 17.5Z" fill="#BD10E0"/>
-    </svg>
-);
-
-export default Ethereum;
